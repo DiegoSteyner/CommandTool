3975a3ba808b3ac8b1cda8e1d39f41f1b5ddbf52
Diego Steyner <diego_steyner@hotmail.com>
2020-01-18 15:56:09 -0300

    Atualização para criar o executável.

--- src/main/java/commandtool/PrincipalCommandTool.java
+++ src/main/java/commandtool/PrincipalCommandTool.java
@@ -14,28 +14,6 @@ public class PrincipalCommandTool {
 		try 
 		{
 			TelaPrincipal.launch(TelaPrincipal.class);
-
-//			extrairArquivos();
-			
-			//https://code.makery.ch/blog/javafx-dialogs-official/
-			
-			// dialog, muita boa explicaÃ§Ã£o.
-			//https://stackoverflow.com/questions/51387453/javafx-how-to-implement-a-busy-and-wait-dialog-routine
-			
-			// outros legais
-			//https://www.dummies.com/programming/java/javafx-binding-properties/
-			
-			// muito legal sobre os componentes
-			//https://o7planning.org/en/11009/javafx
-			
-			// interessante sobre thread
-			//http://aprendendo-javafx.blogspot.com/2014/08/threads-e-javafx.html
-			//https://docs.oracle.com/javase/8/javafx/interoperability-tutorial/concurrency.htm
-			
-			// criar código quebrar os nomes
-			// Criar lógica para setar valores no json em lote
-			
-//			testeMkvToolNix();
 		}
 		catch (Exception e) 
 		{
@@ -62,64 +40,6 @@ public class PrincipalCommandTool {
 		    }
 
 		});
-	}
-	
-	public static void extrairArquivos()
-	{
-		//"C:\Program Files\7-Zip\7z.exe" x "E:\Filmes\filme - a mafia no divã\MNODIV.BLU.1080p.part1.rar" -y -o"E:\Filmes\filme - a mafia no divã"
-		final String sevenZip = "C:\\Program Files\\7-Zip\\7z.exe";
-		final String comandoExtrairAqui = " x \"{0}\"";
-		final String comandoParaDiretorio = " -o\"{0}\"";
-		final String comandoSimParaTodos = " -y";
-		final String diretorioInicial = "E:\\Filmes";
-		
-		try 
-		{
-			LinkedList<Path> arquivos = Files.walk(Paths.get(diretorioInicial)).parallel().filter(p -> p.toString().contains("part1")).collect(Collectors.toCollection(LinkedList::new));
-			
-			arquivos.forEach(p -> {
-				
-				StringBuilder str = new StringBuilder();
-				
-				str.append("\"").append(sevenZip).append("\"");
-				str.append(MessageFormat.format(comandoExtrairAqui, p.toString()));
-				str.append(MessageFormat.format(comandoParaDiretorio, p.getParent().toString()));
-				str.append(comandoSimParaTodos);
-				
-				System.out.println(str.toString());
-				
-				try 
-				{
-//					RuntimeUtils.execSystemCommand(str.toString(), new PrintOutputDTO(System.out), "UTF-8");
-//					RuntimeUtils.execProgramByCommand("\""+sevenZip+"\"", str.toString(), "", new PrintOutputDTO(System.out), "UTF-8");
-				}
-				catch (Exception e) 
-				{
-					e.printStackTrace();
-				}
-				
-//				try(BufferedReader buff = RuntimeUtils.execSystemCommandCmd(str.toString(), "UTF-8"))
-//				{
-//					String line = "";
-//					
-//					while ((line = buff.readLine()) != null)
-//					{
-//						System.out.println(line);
-//					}
-//					
-//					System.out.println("saiu");
-//				} 
-//				catch (Exception e) 
-//				{
-//					e.printStackTrace();
-//				}
-				
-			}) ;
-		}
-		catch (Exception e) 
-		{
-			e.printStackTrace();
-		}
 	}
 	
 	public static void testePid() 
